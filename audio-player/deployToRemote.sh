74849dc01e44a945790064da0b56b0d687d702e1
Peter Motzko <info@pmoscode.de>
2022-06-07 22:21:26 +0200

    feat(ui): Update deploy script

--- audio-player/deployToRemote.sh
+++ audio-player/deployToRemote.sh
@@ -5,4 +5,4 @@ export GOARCH=arm
 export GOARM=5
 
 go build -o testRpi
-sshpass -p "raspberry" scp testRpi pi@10.0.2.80:/home/pi
+sshpass -p "raspberry" scp testRpi pi@10.0.2.80:/home/pi/audio-player
