ede4ed682db2ffa54883c6ae435e3e0a4ea15875
Peter Motzko <info@pmoscode.de>
2022-06-08 23:22:58 +0200

    refactor(rfid-reader): Rearrange deployment script

--- rfid-reader/deployToRemote.sh
+++ rfid-reader/deployToRemote.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-sshpass -p "raspberry" scp rfid-reader.py pi@10.0.2.80:/home/pi
-sshpass -p "raspberry" scp requirements.txt pi@10.0.2.80:/home/pi
+sshpass -p "raspberry" scp rfid-reader.py requirements.txt pi@10.0.2.80:/home/pi/rfid-reader
